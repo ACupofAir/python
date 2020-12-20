5309fac4d10732d1bd4545bc788a8a06d16837ba
ACupofAir <timethoughts@outlook.com>
2020-12-20 08:53:47 +0800

    Ok MilliganDropsPhyExpertment

---
+++ MilliganDrops.py
@@ -0,0 +1,13 @@
+#%%
+import math
+u = float(input("Input U: "))
+t = float(input("Input t: "))
+denominator_1 = t*(1+0.02*math.sqrt(t))
+denominator_2 = u*math.pow(denominator_1,3/2)
+result = 92800/denominator_2
+print("q= ", result)
+print("q/e= ", result/1.60)
+n = round(result/1.60)
+print("n= ", n)
+print("e'= ", result/n)
+print("|e'-e|/e%= ", 100*abs(result/n-1.60)/1.60)
--- temp.py
+++ temp.py
@@ -1,3 +1,31 @@
-x = float(input("Input x: "))
-y = 4.7485*0.001*x - 1.6975*x
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.core.function_base import linspace
+
+plt.plot([100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000],
+         [1.80618, 1.8961, 2.00687, 2.09743, 2.16436, 2.17286, 2.11593, 2.07159, 2.11381, 2.0824, 2.28385, 2.40824])
+
+plt.xlabel('NumOfElements')
+plt.ylabel('Height/log2(NumOfElements')
+plt.ylim(0, 3)
+
+# %%
+x = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+y = [0, 298, 591, 888, 1188, 1490, 1814, 2351, 2403, 3101, 3471]
+plt.plot(x, y)
+plt.xlabel("k")
+plt.ylabel("time/us")
+
+#%%
+y = [i for i in range(0, 1000, 100)]
+x = [i for i in range(1, 11)]
+plt.plot(x, y)
+
+
+#%%
+x = 0.00943
+y = 4.7485*0.001/x -1.6975*x
+print(x)
 print(y)
+# %%
